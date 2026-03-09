658138bd3d7c2cb1ad96fff4c888b55a6230df56
JAFFER NOVA <jaffernova21@gmail.com>
2026-03-10 00:54:05 +0530

    check

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
       <div className="flex flex-col items-center justify-between gap-16 px-4 md:px-10 pb-16 text-xl font-light tracking-wide lg:flex-row-reverse md:text-2xl lg:text-3xl text-white/60">
         <img
           ref={imgRef}
-          src="images/shaed-noor.webp"
+          src="images/"
           alt="shaed noor photo"
           className="rounded-xl w-full lg:w-2/5"
         />
